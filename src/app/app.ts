b7d6b0ba79da79c91f1705a3fdf4958cc08a2148
MOSTAFA-MANSOUR72 <mostafamansour76272@gmail.com>
2026-03-11 13:16:43 +0200

    added task of day1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Slider } from "./components/slider/slider";
+import { Product } from "./components/product/product";
+import { Footer } from "./components/footer/footer";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Slider, Product, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Play_With_Angular');
