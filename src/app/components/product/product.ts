acf5c8941b6ec27c606908d8686fa764b5d3e7f9
MOSTAFA-MANSOUR72 <mostafamansour76272@gmail.com>
2026-03-12 13:43:43 +0200

    updated styling

--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
-export class Product {}
+export class Product {
+  products: any[] = [
+    { id: 1, name: 'Smart Watch', price: 120, image: 'watch1.jpg', inStock: true },
+    { id: 2, name: 'Classic Watch', price: 90, image: 'watch2.jpg', inStock: false },
+    { id: 3, name: 'Sport Watch', price: 150, image: 'watch3.jpg', inStock: true },
+  ];
+}
