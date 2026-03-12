e666308052dc46a3da0c1ba1c86d985fb8e5954f
MOSTAFA-MANSOUR72 <mostafamansour76272@gmail.com>
2026-03-12 14:39:37 +0200

    completed second day task

--- src/app/components/product/product.ts
+++ src/app/components/product/product.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
@@ -11,5 +12,6 @@ export class Product {
     { id: 1, name: 'Smart Watch', price: 120, image: 'watch1.jpg', inStock: true },
     { id: 2, name: 'Classic Watch', price: 90, image: 'watch2.jpg', inStock: false },
     { id: 3, name: 'Sport Watch', price: 150, image: 'watch3.jpg', inStock: true },
+    { id: 4, name: 'Luxury Watch', price: 300, image: 'watch2f.jpg', inStock: true },
   ];
 }
